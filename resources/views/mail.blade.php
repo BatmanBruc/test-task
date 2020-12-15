718d6e30ff7b5f4c59413f4a8331326ba9dce296
Ваше Имя <you@example.com>
2020-12-16 05:19:40 +1000

    Шаблон письма

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,48 +1,62 @@
-<div class="mail">
-    <div class="line">
-        <div class="line-key">Имя</div>
-        <div class="line-value">{{ $name }}</div>
-    </div>
-    <div class="line">
-        <div class="line-key">Email</div>
-        <div class="line-value">{{ $email }}</div>
-    </div>
-    <div class="line">
-        <div class="line-key">Тема</div>
-        <div class="line-value">{{ $theme }}</div>
-    </div>
-    <div class="line">
-        <div class="line-key">Сообщение</div>
-        <div class="line-value">{{ $mess }}</div>
-    </div>
-    <div class="line">
-        <div class="line-key">Файл</div>
-        <div class="line-value"><a href="http://127.0.0.1/uploads/{{ $file }}">Ссылка на файл</a></div>
-    </div>
-    <div class="line">
-        <div class="line-key">Дата</div>
-        <div class="line-value">{{ $date }}</div>
-    </div>
-</div>
-<style>
-    .mail {
-        background: #cee7fd;
+<DOCTYPE html>
+ <html lang=”en-US”>
+ <head>
+ <meta charset=”utf-8">
+ </head>
+ <body>
+ <div class="mail" style="background: #cee7fd;
         padding: 10px;
-        border-radius: 5px;
-    }
-    .line {
-        display: flex;
+        border-radius: 5px;">
+    <div style="display: flex;
         padding: 10px;
         background: #b2cee6;
         border-radius: 5px;
-        line-height: 43px;
-    }
-    .line-key {
-        margin-right: 10px;
-        color: #8c8c8c;
-    }
-    .line-value {
-        color: black;
-        font-size: 20px;
-    }
-</style>
+        line-height: 43px;" class="line">
+        <div style=" margin-right: 10px;
+        color: #8c8c8c;" class="line-key">Имя</div>
+        <div style=" color: black;
+        font-size: 20px;" class="line-value">{{ $name }}</div>
+    </div>
+    <div style="display: flex;
+        padding: 10px;
+        background: #b2cee6;
+        border-radius: 5px;
+        line-height: 43px;" class="line">
+        <div style=" margin-right: 10px;
+        color: #8c8c8c;" class="line-key">Email</div>
+        <div style=" color: black;
+        font-size: 20px;" class="line-value">{{ $email }}</div>
+    </div>
+    <div style="display: flex;
+        padding: 10px;
+        background: #b2cee6;
+        border-radius: 5px;
+        line-height: 43px;" class="line">
+        <div style=" margin-right: 10px;
+        color: #8c8c8c;" class="line-key">Тема</div>
+        <div style=" color: black;
+        font-size: 20px;" class="line-value">{{ $theme }}</div>
+    </div>
+    <div style="display: flex;
+        padding: 10px;
+        background: #b2cee6;
+        border-radius: 5px;
+        line-height: 43px;" class="line">
+        <div style=" margin-right: 10px;
+        color: #8c8c8c;" class="line-key">Сообщение</div>
+        <div style=" color: black;
+        font-size: 20px;" class="line-value">{{ $mess }}</div>
+    </div>
+    <div style="display: flex;
+        padding: 10px;
+        background: #b2cee6;
+        border-radius: 5px;
+        line-height: 43px;" class="line">
+        <div style=" margin-right: 10px;
+        color: #8c8c8c;" class="line-key">Дата</div>
+        <div style=" color: black;
+        font-size: 20px;" class="line-value">{{ $date }}</div>
+    </div>
+</div>
+</body>
+</html>
